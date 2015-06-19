38c2f928f92d2212ee09eb2aa1393bf1a8a6c816
Tawhid <islamtawhid1@gmail.com>
2015-06-19 16:31:36 +0600

    AJAX is implemented, now jquery ui will be done

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart , only: [:create]
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart}
+        format.html { redirect_to store_index_path }
+        format.js   {@current_item = @line_item}
         format.json { render action: 'show',
                              status: :created, location: @line_item }
       else
@@ -67,13 +68,15 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white
+  # list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
+  #...
 end
